1f49cb6a3a5aaa9270c5ce350b509f0ba6b5ca8c
Matt <m.connell97@hotmail.co.uk>
2021-02-05 12:42:24 +0000

    Added dice roll function
    Generates random number between 1-6 and updates dice image on button click

--- dicegame1/script.js
+++ dicegame1/script.js
@@ -1,2 +1,14 @@
-
 //fuctions: roll, change image, add up score, restart
+const score = document.querySelector("#score");
+const diceImg = document.querySelector("#dice-img");
+const rollBtn = document.querySelector("#roll-btn");
+
+function rollDice() {
+    let roll = Math.ceil(Math.random()*6);
+    console.log(roll);
+    diceImg.src = (`img/dice${roll}.png`);
+};
+
+rollBtn.addEventListener("click", () => {
+    rollDice();
+});
